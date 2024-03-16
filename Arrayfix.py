977dd66f4971cbaefc0532e909e7307178e298c6
27Gaurav <97743401+27Gaurav@users.noreply.github.com>
2024-03-16 23:32:25 +0530

    Add files via upload

---
+++ Arrayfix.py
@@ -0,0 +1,45 @@
+n= int(input())
+for t in range(n) :
+    m= int(input())
+    l= list(map(int,input().split()))
+    if l[0]<10:
+        last = l[0]
+    else :
+        s= str(l[0])
+        
+        if int(s[0])<=int(s[1]):
+ 
+            last = int(s[1])
+        else:
+            last = l[0]
+    ans ="YES"
+    for i in range(1,m):
+        if l[i]<10:
+            if l[i]>= last :
+                last = l[i]
+            else: 
+                ans = "NO"
+                break
+        else : 
+            i1 = int(str(l[i])[0])
+            i2 = int(str(l[i])[1])
+        
+            if i1<=i2 :
+                if i1>= last:      
+                    last = i2
+                else: 
+                    if l[i]>= last :
+                        last = l[i]
+                    else:
+                        ans = "NO"
+                        break
+ 
+            else:
+                if l[i]>=last :
+                    last = l[i]
+                else:
+                     ans = "NO"
+                     break
+    print(ans)
+ 
+            
